50c4dd604f6d5f002a5d98ffdacd09b829e00ea5
Chaya Hershkovits <ChayaH@skyvar.co.il>
2023-07-02 10:28:51 +0300

    upload slack api

---
+++ api.js
@@ -0,0 +1,67 @@
+const { WebClient } = require('@slack/web-api');
+
+const token = 'xoxp-5492487209540-5475511713527-5495878040370-a4d9e2c9ea4ac486dffb963383066fa9';
+
+const web = new WebClient(token);
+
+write().catch(err => console.log(err));
+create().catch(err => console.log(err));
+add().catch(err => console.log(err));
+remove().catch(err => console.log(err));
+
+////write message to a channel
+const conversationId = 'test';
+
+async function write() {
+    const result = await web.chat.postMessage({
+        text: 'Hello world!2',
+        channel: conversationId,
+    });
+    console.log(`Successfully send message ${result.ts} in conversation ${conversationId}`);
+};
+
+
+////create a new channel
+async function create() {
+    try {
+        const result = await web.conversations.create({
+            name: "try"
+        }); 
+        console.log(result);
+    }
+    catch (error) {
+        console.error(error);
+    }   
+}
+
+
+////add user to channel
+async function add() {
+    try {   
+        const result = await web.conversations.invite({
+            token,
+            channel:'C05EEEWCSAJ',
+            users: 'U05EE2000QJ'
+        });        
+        console.log(result);
+    }
+    catch (error) {
+        console.error(error);
+    }   
+}
+
+
+////remove people from a channel
+async function remove() {
+    try {    
+        const result = await web.conversations.kick({
+            token,
+            channel:'C05EEEWCSAJ',
+            user: 'U05ESLKMJRF'
+        }); 
+        console.log(result);
+    }
+    catch (error) {
+        console.error(error);
+    }   
+}
