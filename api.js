b8ce2d91ff02b6cba27f367125695a6b1b3f7e06
Chaya Hershkovits <ChayaH@skyvar.co.il>
2023-07-20 12:40:48 +0300

    changes

--- api.js
+++ api.js
@@ -16,7 +16,7 @@ const config = yaml.load(fs.readFileSync('./.github/workflows/github-actions.yml
 const indentedJson = JSON.stringify(config, null, 4);
     // console.log(indentedJson);
 console.log(typeof(config.env.TOKEN));
-console.log(config.env.TOKEN);
+console.log(config.env.TOKEN.value);
     // for (const a of config.definitions.locations){
     //     console.log(a);
     // }
